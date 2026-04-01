a69c915aaf763586431a72f8a35f2bdbec93a607
NanoDev <b.rego.oliveira@gmail.com>
2026-04-01 14:31:25 +0100

    simple bootloader

---
+++ terra-kernel/bootloader/bootloader.asm
@@ -0,0 +1,27 @@
+.org 0x7C00
+section .text
+
+.global start
+start:
+    xor ax, ax
+    mov si, [str] 
+
+print:
+    xor bx, bx
+    mov ah, 0x0E
+    
+    mov al, [si]
+    cmp al, 0
+    je l1
+
+    int 0x10
+    inc si
+    jmp print
+
+l1: ret
+
+str:
+    .asciz "Sup Homie"
+
+.org 510
+.word 0xAA55
