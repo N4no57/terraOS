1e253ed6276360a3a5d2dfa8f24afb2169e4addd
NanoDev <b.rego.oliveira@gmail.com>
2026-04-03 13:41:49 +0100

    tiny mistake fixed

--- terra-kernel/kernel/main.c
+++ terra-kernel/kernel/main.c
@@ -3,7 +3,7 @@ void kernel_main(void) {
     unsigned short *VGA_MEMORY = (unsigned short*)0xB8000;
     const char message[] = "Hello, TerraOS!\0";
     for (int i = 0; message[i] != '\0'; i++) {
-        VGA_MEMORY[i] = 0xF000 | message[i]; // white text on black background
+        VGA_MEMORY[i] = 0x0F00 | message[i]; // white text on black background
     }
     while (1) {
         // halt
