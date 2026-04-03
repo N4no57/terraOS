94b59c295baf1d1d6fe765e91711d2b6002c5e95
NanoDev <b.rego.oliveira@gmail.com>
2026-04-03 23:17:28 +0100

    changes to turn this into a higher half kernel

--- terra-kernel/bootloader/boot_funcs/protected_mode/init_pt.asm
+++ terra-kernel/bootloader/boot_funcs/protected_mode/init_pt.asm
@@ -10,14 +10,23 @@ init_pt:
     rep stosd
 
     ; set edi to PML4T[0]
-    mov edi, cr3
+    mov edi, 0x1000
+    add edi, 511 * 8 ; move to PML4T[511]
 
     ; set up the first entry of each table
-    mov dword [edi], 0x2003 ; Set PML4T[0] to address 0x2000 (PDPT) with flags 0x0003
-    add edi, 0x1000 ; move to PDPT[0]
-    mov dword [edi], 0x3003 ; Set PDPT[0] to address 0x3000 (PDT) with flags 0x0003
-    add edi, 0x1000 ; move to PDT[0]
+    mov dword [edi], 0x2003 ; Set PML4T[511] to address 0x2000 (PDPT) with flags 0x0003
+    mov dword [edi + 8], 0
+
+    mov edi, 0x2000 ; set to PDPT[0]
+    add edi, 510 * 8 ; move to PDPT[510]
+
+    mov dword [edi], 0x3003 ; Set PDPT[510] to address 0x3000 (PDT) with flags 0x0003
+    mov dword [edi + 8], 0
+
+    mov edi, 0x3000 ; move to PDT[0]
+
     mov dword [edi], 0x4003 ; Set PDT[0] to address 0x4000 (PT) with flags 0x0003
+    mov dword [edi + 8], 0
 
     ; fill in the page table
     add edi, 0x1000         ; move to PT[0]
@@ -26,6 +35,7 @@ init_pt:
 
     add_page_entry_protected:
         mov dword [edi], ebx ; set page table entry to current address with flags
+        mov dword [edi + 4], 0 ; clear upper 32 bits of entry
         add ebx, 0x1000     ; next page (4KB)
         add edi, 8 ; move to next page table entry
         loop add_page_entry_protected
