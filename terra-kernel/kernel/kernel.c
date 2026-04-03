20e764eaeff3a98b4f864a4c7e748f4c61d8ce17
NanoDev <b.rego.oliveira@gmail.com>
2026-04-03 19:49:55 +0100

    renamed main.c to kernel.c

--- terra-kernel/kernel/kernel.c
+++ terra-kernel/kernel/kernel.c
@@ -1,11 +1,17 @@
-void kernel_main(void) __attribute__((section(".text")));
 void kernel_main(void) {
     unsigned short *VGA_MEMORY = (unsigned short*)0xB8000;
-    const char message[] = "Hello, TerraOS!\0";
+    const char message[] = "TerraOS - 64-bit C Kernel\0";
+
     for (int i = 0; message[i] != '\0'; i++) {
         VGA_MEMORY[i] = 0x0F00 | message[i]; // white text on black background
     }
+
+
+    for (int i = 0; i < 80 * 25; i++) {
+        VGA_MEMORY[i] = 0x0F00 | ' ';
+    }
+
     while (1) {
-        // halt
+        __asm__ volatile ("hlt");
     }
 }
