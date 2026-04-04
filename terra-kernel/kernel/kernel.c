abed336ac39b80554cb6e81a044297c973a96bb7
NanoDev <b.rego.oliveira@gmail.com>
2026-04-04 11:23:19 +0100

    idk tf going on here

--- terra-kernel/kernel/kernel.c
+++ terra-kernel/kernel/kernel.c
@@ -1,14 +1,15 @@
 void kernel_main(void) {
     unsigned short *VGA_MEMORY = (unsigned short*)0xB8000;
+    int y = 11;
     const char message[] = "TerraOS - 64-bit C Kernel\0";
 
     for (int i = 0; message[i] != '\0'; i++) {
-        VGA_MEMORY[i] = 0x0F00 | message[i]; // white text on black background
+        VGA_MEMORY[i + y * 80] = 0x0F00 | message[i]; // white text on black background
     }
 
 
     for (int i = 0; i < 80 * 25; i++) {
-        VGA_MEMORY[i] = 0x0F00 | ' ';
+        // VGA_MEMORY[i] = 0x0F00 | ' ';
     }
 
     while (1) {
