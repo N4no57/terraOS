9c91a7b92d998f56129fe43c88ca720da4ec0ada
NanoDev <b.rego.oliveira@gmail.com>
2026-04-04 22:57:45 +0100

    end my life once more

--- terra-kernel/kernel/idt/idt.c
+++ terra-kernel/kernel/idt/idt.c
@@ -43,7 +43,7 @@ extern void irq1(void);
 
 void idt_set_gate(int n, uint64_t handler) {
     idt[n].offset_low = handler & 0xFFFF;
-    idt[n].selector = 0x08;  // Kernel code segment
+    idt[n].selector = 0x18;  // Kernel code segment
     idt[n].ist = 0;
     idt[n].type_attr = 0x8E; // Present, Ring 0, Interrupt Gate
     idt[n].offset_mid = (handler >> 16) & 0xFFFF;
