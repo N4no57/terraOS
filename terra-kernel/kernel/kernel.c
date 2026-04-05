c0de2af2d631628071dcb7af92005891f2742aef
NanoDev <b.rego.oliveira@gmail.com>
2026-04-05 23:22:01 +0100

    more refactoring

--- terra-kernel/kernel/kernel.c
+++ terra-kernel/kernel/kernel.c
@@ -173,21 +173,3 @@ void* alloc_page() {
 u64 v2p(void* v) {
     return (u64)v - KERNEL_BASE;
 }
-
-void* memcpy(void* dest, const void* src, size_t count) {
-    char* d = dest;
-    const char* s = src;
-    for (int i = 0; i < count; i++) {
-        d[i] = s[i];
-    }
-    return dest;
-}
-
-void *memset(void* dest, int c, size_t count) {
-    u8 *ptr = (u8*) dest;
-    u8 byte_val = (u8) c;
-    for (size_t i = 0; i < count; i++) {
-        ptr[i] = byte_val;
-    }
-    return dest;
-}
